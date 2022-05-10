8b95ea9c10ece19f6228e8db701b354ccd75f36c
Prasad Acharya <50115378+prasad145@users.noreply.github.com>
2022-05-10 23:56:28 +0530

    Create 2006. Count Number of Pairs With Absolute Difference K.cpp

---
+++ 2006. Count Number of Pairs With Absolute Difference K.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        set<int> freq;
+        int ans = 0;
+        for(int i = 0; i < nums.size() - 1; i++)
+        {
+            for(int j = i + 1; j < nums.size(); j++)
+            {
+                if(abs(nums[i] - nums[j]) == k)
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
